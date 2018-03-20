c4cdda869bdf0411d0f55e299ec0423967657a2d
smalltide <smalltides@gmail.com>
2018-03-20 13:02:12 +0800

    revise 1

--- main.go
+++ main.go
@@ -125,7 +125,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // tmpl is the HTML template that drives the user interface.
 var tmpl = template.Must(template.New("tmpl").Parse(`
 <!DOCTYPE html><html><body><center>
-	<h2>Is Go {{.Version}} out yet?</h2>
+	<h2>Is Go {{.Version}} out yet!!!</h2>
 	<h1>
 	{{if .Yes}}
 		<a href="{{.URL}}">YES!</a>
